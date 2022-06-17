5498fa90ff7531385cba3538b15c2349e5196c9e
sofia-kildyaeva <81563074+sofia-kildyaeva@users.noreply.github.com>
2022-06-17 02:34:41 -0700

    Tenth lab

---
+++ Lab 10/Lab A/Ex1/Starter/MeasuringDevice/IMeasuringDeviceWithProperties.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MeasuringDevice
+{
+    interface IMeasuringDeviceWithProperties : ILoggingMeasuringDevice
+    {
+        Units UnitsToUse { get; }
+        int[] DataCaptured { get; }
+        int MostRecentMeasure { get; }
+        string LoggingFileName { get; set; }
+    }
+}
