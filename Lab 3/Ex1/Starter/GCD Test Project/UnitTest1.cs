a845cca4ddfad10ac393901c14954afeee5e104f
sofia-kildyaeva <81563074+sofia-kildyaeva@users.noreply.github.com>
2022-06-17 01:52:35 -0700

    Third lab

---
+++ Lab 3/Ex1/Starter/GCD Test Project/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GCD_Test_Project
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        public void TestMethod1()
+        {
+        }
+    }
+}
