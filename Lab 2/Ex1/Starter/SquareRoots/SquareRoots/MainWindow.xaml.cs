54bd2979d8d1219f29b665f328708b68efc5cbc7
sofia-kildyaeva <81563074+sofia-kildyaeva@users.noreply.github.com>
2022-06-17 01:12:01 -0700

    Second lab

---
+++ Lab 2/Ex1/Starter/SquareRoots/SquareRoots/MainWindow.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SquareRoots
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void calculateButton_Click(object sender, RoutedEventArgs e)
+        {
+            double numberDouble;
+            if (!double.TryParse(inputTextBox.Text, out numberDouble))
+            {
+                MessageBox.Show("Please enter a double");
+                return;
+            }
+
+            if (numberDouble <= 0)
+            {
+                MessageBox.Show("Please enter a positive number");
+                return;
+            }
+
+            double squareRoot = Math.Sqrt(numberDouble);
+
+            frameworkLabel.Content = string.Format("{0} (Using the .NET Framework)", squareRoot);
+
+            decimal numberDecimal;
+            if (!decimal.TryParse(inputTextBox.Text, out numberDecimal))
+            {
+                MessageBox.Show("Please enter a decimal");
+                return;
+            }
+
+            decimal delta = Convert.ToDecimal(Math.Pow(10, -28));
+
+            decimal guess = numberDecimal / 2;
+
+            decimal result = ((numberDecimal / guess) + guess / 2);
+
+            while (Math.Abs(result - guess) > delta)
+            {
+                guess = result;
+
+                result = ((numberDecimal / guess) + guess) / 2;
+            }
+
+            newtonLabel.Content = string.Format("{0} (Using Newton)", result);
+        }
+    }
+}
---
+++ Lab 2/Ex2/Starter/IntegerToBinary/IntegerToBinary/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace IntegerToBinary
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void convertButton_Click(object sender, RoutedEventArgs e)
+        {
+            int i;
+            if (!int.TryParse(inputTextBox.Text, out i))
+            {
+                MessageBox.Show("TextBox does not contain an integer");
+                return;
+            }
+
+            if (i < 0)
+            {
+                MessageBox.Show("Please enter a positive number or zero");
+                return;
+            }
+
+            int remainder = 0;
+
+            StringBuilder binary = new StringBuilder();
+
+            do
+            {
+                remainder = i % 2;
+                i = i / 2;
+                binary.Insert(0, remainder);
+            }
+            while (i > 0);
+
+            binaryLabel.Content = binary.ToString();
+        }
+    }
+}
